9907affdf210ad14d646b275fff9bbaca369eeb6
Ananda018 <ghosh18ananda@gmail.com>
2025-08-19 18:19:38 +0530

    dataBase connection successfully done

---
+++ src/db/index.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+
+const connectDB= async () => {
+  try {
+    // Connect to MongoDB
+    const connectionInstance= await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+    console.log(`Database connected successfully to ${connectionInstance.connection.host}`);
+
+  } catch (error) {
+    console.error("Database connection error:", error);
+    process.exit(1);
+  }};
+
+
+export default connectDB;
